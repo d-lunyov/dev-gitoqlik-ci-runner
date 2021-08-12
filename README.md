79169bdbcb946e7948a71b3086db7a38160aec1a
Dmitriy Lunyov <dm.lunyov@yandex.ru>
2021-08-12 16:18:37 +0300

    Add action

---
+++ README.md
@@ -0,0 +1,1 @@
+## Public Gitoqlik CI action
--- configReader.js
+++ configReader.js
@@ -1,18 +1,16 @@
 const fs = require("fs");
 const path = require("path");
-const log = require(`./logger`).log;
+const resolvePath = (filename) =>  path.resolve(__dirname, `./`, filename);
 
 getQlikServers = function () {
-    const resolveConfigPath = (filename) =>  path.resolve(__dirname, `./`, filename);
-    const readConfig = (filename) => fs.readFileSync(resolveConfigPath(filename));
+    const readConfig = (filename) => fs.readFileSync(resolvePath(filename));
 
     const config = JSON.parse(readConfig(`config.json`));
     return config.qlikServers || [];
 }
 
 getCertificate = function(filePath) {
-    const resolveCertPath = (filename) =>  path.resolve(__dirname, `./`, filename);
-    const readCert = (filename) => fs.readFileSync(resolveCertPath(filename));
+    const readCert = (filename) => fs.readFileSync(resolvePath(filename));
 
     return readCert(filePath);
 }
