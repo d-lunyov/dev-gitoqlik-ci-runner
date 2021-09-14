29a6cfcd1420659a192a73816d99c21d0cbfa7fe
Dmitriy Lunyov <dm.lunyov@yandex.ru>
2021-09-14 12:25:00 +0300

    Fix CreateJWT script

--- PowerShell/CreateJWTVirtualProxy.ps1
+++ PowerShell/CreateJWTVirtualProxy.ps1
@@ -42,7 +42,7 @@ if (-not ($continueResponse -eq "y")) {
 
 Write-Output "Connectig to the Qlik Server..."
 try {
-    Connect-Qlik localhost
+    Connect-Qlik localhost -TrustAllCerts
     #Get-PfxCertificate "C:\PS\_cert\dev2.datanomix.pro_pfx\client.pfx" | Connect-Qlik "QS-DEV.datanomix.local" -UserName "DATANOMIX.PRO\teacher@datanomix.pro" -TrustAllCerts
 }
 catch {
@@ -69,7 +69,7 @@ $jwt_jwtAttributeUserId = "userId"
 $jwt_jwtAttributeUserDirectory = "userDirectory"
 
 Write-Output "Creating Virtual Proxy..."
-$createdVirtualProxy = New-QlikVirtualProxy -prefix $jwt_prefix -description $jwt_description -sessionCookieHeaderName $jwt_sessionCookieHeaderName -loadBalancingServerNodes $jwt_loadBalancingServerNodes -websocketCrossOriginWhiteList $jwt_websocketCrossOriginWhiteList -authenticationMethod 'JWT' -jwtPublicKeyCertificate $jwt_jwtPublicKeyCertificate -jwtAttributeUserId $jwt_jwtAttributeUserId -jwtAttributeUserDirectory $jwt_jwtAttributeUserDirectory
+$createdVirtualProxy = New-QlikVirtualProxy -prefix $jwt_prefix -description $jwt_description -sessionCookieHeaderName $jwt_sessionCookieHeaderName -loadBalancingServerNodes $jwt_loadBalancingServerNodes -websocketCrossOriginWhiteList $jwt_websocketCrossOriginWhiteList -authenticationMethod 'JWT' -jwtPublicKeyCertificate "$jwt_jwtPublicKeyCertificate" -jwtAttributeUserId $jwt_jwtAttributeUserId -jwtAttributeUserDirectory $jwt_jwtAttributeUserDirectory
 Write-Output "Success, id: $($createdVirtualProxy.id)"
 
 Write-Output "Linking Virtual Proxy to the Central Proxy..."
@@ -89,16 +89,178 @@ Write-Output "Updating Central Proxy..."
 Update-QlikProxy -id $qlik_centralProxy.id -virtualProxies $centralProxy_virtualProxies
 
 # ======================================= Generate JWT Section =====================================
-Write-Output "Installing modules to generate JWT..."
-if (-not (Get-Module -ListAvailable -Name powershell-jwt)) {
-    Install-Module powershell-jwt -Force
+if ([int]$PSVersionTable.PSVersion.Major -gt 5) {
+    $jwt_privateKey =  Get-Content -Path  "$($jwt_certificatePath)\client_key.pem" -AsByteStream
+} else {
+    $jwt_privateKey =  Get-Content -Path  "$($jwt_certificatePath)\client_key.pem" -Encoding Byte -Raw
 }
+
+Write-Output "Installing modules to generate JWT..."
+if (-not (Get-Module -ListAvailable -Name BAMCIS.Crypto)) {
+    Install-Module BAMCIS.Crypto -Force
+}
+
+Import-Module -Name BAMCIS.Crypto
+
+Function New-JwtHeader {
+    param (
+        [Parameter(Mandatory = $True)]
+        [string]
+        $Algorithm,
+
+        [string]
+        $Type = 'JWT',
+
+        [hashtable]
+        $ExtraClaims = @{}
+    )
+
+    $header = @{
+        alg = $Algorithm
+        typ = $Type
+    } + $ExtraClaims
+
+    $header
+}
+
+Function New-JwtPayload {
+    param (
+        [Parameter(Mandatory = $True)]
+        [string]
+        $Issuer,
+
+        [Parameter(Mandatory = $True)]
+        [int]
+        $ExpiryTimestamp,
+
+        [hashtable]
+        $ExtraClaims = @{}
+    )
+
+    $payload = @{
+        iss = $Issuer
+        exp = $ExpiryTimestamp
+    } + $ExtraClaims
+
+    $payload
+}
+Function Convert-HashtableToJsonBase64 {
+    param (
+        [Parameter(Mandatory = $True)]
+        [hashtable]
+        $Hashtable
+    )
+
+    $json = $Hashtable | ConvertTo-Json -Compress
+    $jsonbase64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($json)).Split('=')[0].Replace('+', '-').Replace('/', '_')
+
+    $jsonbase64
+}
+
+Function Convert-JsonBase64ToHashtable {
+    param (
+        [Parameter(Mandatory = $True)]
+        [string]
+        $JsonBase64
+    )
+
+    $JsonBase64 = $JsonBase64 -replace '-', '+' -replace '_', '/'
+    switch ($JsonBase64.Length % 4) {
+        0 { break }
+        2 { $JsonBase64 += '==' }
+        3 { $JsonBase64 += '=' }
+    }
+
+    $json = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($JsonBase64))
+    $hashtable = $json | ConvertFrom-Json -AsHashtable
+
+    $hashtable
+}
+Function Get-SignatureRS {
+    param (
+        [Parameter(Mandatory = $True)]
+        [string]
+        $Algorithm,
+
+        [Parameter(Mandatory = $True)]
+        [System.Byte[]]
+        $SecretKey,
+
+        [Parameter(Mandatory = $True)]
+        [string]
+        $ToBeSigned
+    )
+
+    $SigningAlgorithm = switch ($Algorithm) {
+        "RS256" {[Security.Cryptography.HashAlgorithmName]::SHA256}
+        "RS384" {[Security.Cryptography.HashAlgorithmName]::SHA384}
+        "RS512" {[Security.Cryptography.HashAlgorithmName]::SHA512}
+        Default {Write-Error -Message ('Unsupported algorithm: ' + $Algorithm)}
+    }
+
+    $rsa = ConvertFrom-PEM -PEM ([System.Text.Encoding]::UTF8.GetString($SecretKey))
+
+    $Signature = [Convert]::ToBase64String(
+        $rsa.SignData(
+            [System.Text.Encoding]::UTF8.GetBytes($ToBeSigned),
+            $SigningAlgorithm,
+            [Security.Cryptography.RSASignaturePadding]::Pkcs1
+        )
+    ).Split('=')[0].Replace('+', '-').Replace('/', '_')
+
+    $Signature
+}
+Function New-JWT {
+    param (
+        [Parameter(Mandatory = $True)]
+        [string]
+        $Algorithm,
+
+        [string]
+        $Type = 'JWT',
+
+        [hashtable]
+        $HeaderClaims = @{},
+
+        [Parameter(Mandatory = $True)]
+        [string]
+        $Issuer,
+
+        [Parameter(Mandatory = $True)]
+        [int]
+        $ExpiryTimestamp,
+
+        [hashtable]
+        $PayloadClaims = @{},
+
+        [Parameter(Mandatory = $True)]
+        [System.Byte[]]
+        $SecretKey
+    )
+
+    $header = New-JwtHeader -Algorithm $Algorithm -Type $Type -ExtraClaims $HeaderClaims
+    $payload = New-JwtPayload -Issuer $Issuer -ExpiryTimestamp $ExpiryTimestamp -ExtraClaims $PayloadClaims
+
+    $headerBase64 = Convert-HashtableToJsonBase64 -Hashtable $header
+    $payloadBase64 = Convert-HashtableToJsonBase64 -Hashtable $payload
+
+    $ToBeSigned = $headerBase64 + "." + $payloadBase64
+    $signature = switch -Wildcard ($Algorithm) {
+        'RS???' { Get-SignatureRS -Algorithm $Algorithm -SecretKey $SecretKey -ToBeSigned $ToBeSigned }
+        Default { Write-Error -Message ('Unsupported algorithm: ' + $Algorithm) }
+    }
+
+    $token = $ToBeSigned + "." + $signature
+    $token
+}
+
+$exp = [DateTimeOffset]::Now.ToUnixTimeSeconds() + 157680000 # 5 years
 
 $jwtPayload = @{
     userId = $qlik_userId
     userDirectory = $qlik_userDirectory
 }
 
-$jwt = New-JWT -Algorithm 'RS256' -Issuer 'Gitoqlok' -SecretKey $jwt_jwtPublicKeyCertificate -ExpiryTimestamp $exp -PayloadClaims $jwtPayload
+$jwt = New-JWT -Algorithm 'RS256' -Issuer 'Gitoqlok' -SecretKey $jwt_privateKey -ExpiryTimestamp $exp -PayloadClaims $jwtPayload
 
 Write-Output "Virtual Proxy with JWT auth created successfully. Copy JWT for use in the Gitoqlok CI configuration:" $jwt
